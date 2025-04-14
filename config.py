34b77e2c1a59e933e4a397210727e6afae5bcc18
Vinicius.Crypto <viniciusprados@discente.ufg.br>
2025-04-14 09:36:45 -0300

    refactor: simplify SQLALCHEMY_DATABASE_URI assignment in config.py

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "XFmkbizvA2gL"
-
-    # Banco de dados via variável de ambiente (segurança e flexibilidade)
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///db.sqlite3"
-
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     UPLOAD_FOLDER = os.path.join(os.getenv('HOME', ''), 'site/wwwroot/uploads/termos')
 
 class DevelopmentConfig(Config):
